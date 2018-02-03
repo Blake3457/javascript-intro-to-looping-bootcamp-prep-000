77ff27cecee77de8a6742daf565949119ae04dea
Blake Kunishige <blake3457@yahoo.com>
2018-02-03 05:14:16 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -20,7 +20,7 @@ function doWhileLoop(array) {
     return Math.random() >= 0.5
     do {
       array.pop();
-    } while (array.length > -1 && maybeTrue())
+    } while (array.length > -1 && maybeTrue());
   }
   
 }
